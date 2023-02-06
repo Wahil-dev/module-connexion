fd49a4cbc880e4f76881bf018ff3acae6a60649b
Wahil-dev <wahil.chettouf@laplateforme.io>
2023-02-06 13:08:57 +0100

    connexion page style structure

--- index.php
+++ index.php
@@ -3,16 +3,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="fr-FR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceuil</title>
-    <style>
-        <?php include $pathInclude."assets/css/style.css" ?>
-        <?php include $pathInclude."assets/css/MQuiry.css" ?>
-    </style>
-</head>
+    <?php require_once($pathInclude."inc/head.php");?>
 <body>
     <div class="content">
         <?php require_once($pathInclude."inc/header.php");?>
